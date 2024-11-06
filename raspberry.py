8a9c1248380d93d1213077705856c26d25737931
0pss <patrick.stecher@tum.de>
2024-11-06 22:21:10 +0100

    new command structure, busy wait utilization preparation3

--- raspberry.py
+++ raspberry.py
@@ -62,7 +62,7 @@ class I2CController:
                 print("sending position: ", position, "and data:", data)
                 self.bus.write_i2c_block_data(self.address, 0x01, [data])
 
-                self.bus.write_i2c_block_data(self.address, 0x02, [0])
+                self.bus.write_i2c_block_data(self.address, 0x02, [data])
 
 
         except Exception as e:
