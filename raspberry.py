82097931eead3deff051cb189f9ca4b1e108a602
0pss <patrick.stecher@tum.de>
2024-11-12 19:56:14 +0100

    new i2c read function

--- raspberry.py
+++ raspberry.py
@@ -177,7 +177,7 @@ class I2CController:
                 self.bus.i2c_rdwr(msg)
                 
                 #print(high_byte)
-                print(msg)
+                print(bin(msg))
                 self.current_bpm = 120 + 0
 
                 #### END BPM ###########
